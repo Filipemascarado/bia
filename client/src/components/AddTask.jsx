5ab34cc17415c4fb6ffd104938888ebdea480729
filipealvesmachado@gmail.com <ec2-user@ip-172-31-2-22.ec2.internal>
2026-08-08 21:46:10 +0000

    Ajuste no botão AddTask

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add New Task
+        Adicionar Nova Tarefa - deploy com ia no Kiro Cli
       </button>
       
       <Modal
